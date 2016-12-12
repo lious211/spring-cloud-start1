5f95dfaadad17fe50cd5db6820da34906f0ee6e5
lisubin <lisubin@heika.com>
2016-12-12 12:53:12 +0800

    add

--- cloud-mybatis-server1/src/main/java/com/abin/lee/cloud/mybatis/server/CloudMybatisServerApplication.java
+++ cloud-mybatis-server1/src/main/java/com/abin/lee/cloud/mybatis/server/CloudMybatisServerApplication.java
@@ -1,9 +1,16 @@
 package com.abin.lee.cloud.mybatis.server;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 /**
  * Created by abin on 2016/12/11 2016/12/11.
  * spring-cloud-start1
  * com.abin.lee.cloud.mybatis.server
  */
+@SpringBootApplication
 public class CloudMybatisServerApplication {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(CloudMybatisServerApplication.class).web(true).run(args);
+    }
 }
